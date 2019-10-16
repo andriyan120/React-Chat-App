95fcd5ca16a550e5d352e432401f149f3d2923fe
andriyan120 <andrieyean146@gmail.com>
2019-10-16 09:15:07 +0700

    feat: add samples chat

---
+++ src/samples/contacts.js
@@ -0,0 +1,40 @@
+const contacts = [
+    {
+        id: 'abc101',
+        title: 'Vasel',
+        meta: ':)',
+        image: './public/images/cat8.jpg'
+    },
+    {
+        id: 'abc102',
+        title: 'Khabib',
+        image: './public/images/cat4.jpg'
+    },
+    {
+        id: 'abc103',
+        title: 'Jeni',
+        image: './public/images/cat1.jpg',
+    },
+    {
+        id: 'abc104',
+        title: 'Eko',
+        image: '../public/images/cat2.jpg'
+    },
+    {
+        id: 'abc105',
+        title: 'Aceng',
+        meta: 'aduh',
+        image: './public/images/cat5.jpg'
+    },
+    {
+        id: 'abc106',
+        title: 'John',
+        image: './public/images/cat6.jpg'
+    },
+    {
+        id: 'abc107',
+        title: 'Ato',
+        image: './public/images/cat7.jpg'
+    }
+]
+export default contacts;
---
+++ src/samples/messages.js
@@ -0,0 +1,33 @@
+const messages = {
+    'abc101': {
+        title: 'Vasel',
+        meta: 'Apa Kabar ',
+        date: 'Sep 20 2019',
+        image: './public/images/cat8.jpg',
+        messages: [
+            {
+                type: 'badge',
+                content: 'Sep 15 1019, 08:32'
+            },
+            {
+                type: 'badge',
+                content: 'you joined the conversation'
+            },
+            {
+                type: 'message',
+                content: 'Assalamualaikum',
+                meta: 'vasel',
+                date: 'Sep 20 2019, 11:21'
+            },
+            {
+                type: 'message',
+                content: 'Apa Kabar',
+                meta: 'vasel',
+                date: 'Sep 20 2019, 11:21'
+            }
+        ]
+
+    }
+}
+
+export default messages;
