04a374d0cafe2be3731689ead4df5cfb993fc0bd
andriyan120 <andrieyean146@gmail.com>
2019-10-16 13:37:55 +0700

    Add Sidebar, MainWindow and redux store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux'
+import { HashRouter as Router } from 'react-router-dom'; 
+
+
+import { initiateContacts } from './redux/actions/ContactsActions';
+import { initiateMessages } from './redux/actions/MessagesActions';
+import configureStore from './configureStore';
+
+import './App.scss'
+import Sidebar from './containers/Sidebar';
+import MainWindow from './containers/MainWindow';
+
+import contacts from './samples/contacts';
+import messages from './samples/messages';
+
+
+const store = configureStore()
+store.dispatch(initiateContacts(contacts));
+store.dispatch(initiateMessages(messages));
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	  <Provider store={store}>
+		<Router>
+			  <div className='app'>
+				  <Sidebar />
+				  <MainWindow />
+			</div>
+		</Router>
+	  </Provider>
   );
 }
 
